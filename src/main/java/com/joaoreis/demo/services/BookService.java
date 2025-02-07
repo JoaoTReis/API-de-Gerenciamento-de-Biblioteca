0d1e57e55ca3f3c2b2705098e43432ca7813974b
Joao Reis <joaoteireis@gmail.com>
2025-02-07 15:38:35 -0300

    Exception handling - update

--- src/main/java/com/joaoreis/demo/services/BookService.java
+++ src/main/java/com/joaoreis/demo/services/BookService.java
@@ -3,8 +3,11 @@ package com.joaoreis.demo.services;
 import com.joaoreis.demo.entities.Book;
 import com.joaoreis.demo.entities.User;
 import com.joaoreis.demo.repositories.BookRepository;
+import com.joaoreis.demo.services.exceptions.DatabaseException;
 import com.joaoreis.demo.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -30,6 +33,12 @@ public class BookService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 }
